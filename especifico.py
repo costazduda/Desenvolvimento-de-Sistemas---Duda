f33844d3f5db19eb39d0519daa9d2fb1f1f75b04
costazduda <dudaduddinha1407@gmail.com>
2026-04-01 12:15:43 -0300

    Add files via upload

---
+++ especifico.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome: ")
+
+if nome == "João":
+    print("Bem-vindo João")
+else:
+    print("Olá")
---
+++ porta.py
@@ -0,0 +1,6 @@
+porta = input("A porta está aberta? (sim/não)")
+
+if porta == "sim":
+    print("Entre")
+else: 
+    print("Espere")
