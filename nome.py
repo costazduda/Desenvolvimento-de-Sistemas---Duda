6d16ba7651a81242ff3582497a493e5f1100610b
costazduda <dudaduddinha1407@gmail.com>
2026-04-08 13:03:07 -0300

    Add files via upload

---
+++ nome.py
@@ -0,0 +1,7 @@
+nome = (input("Digite seu nome: "))
+idade = int(input("Digite sua idade: "))
+
+print("Olá", nome)
+print("Você tem", idade, "anos de idade")
+
+print("Profesorra isso foi só um teste para ver se conseguia KKK")
