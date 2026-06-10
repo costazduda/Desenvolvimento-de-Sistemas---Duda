d86c252e5c88b2af3f3900afd2c423629d1e15d1
costazduda <dudaduddinha1407@gmail.com>
2026-06-10 12:59:26 -0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,4 @@
+nome=input("Digite seu nome:") 
+idade=int(input("Digite sua idade:")) #Variável inteira
+print("Seu nome é:",nome) #Saída nome da pessoa
+print("Sua idade é:",idade) #Saída idade da pessoa
---
+++ 2.py
@@ -0,0 +1,5 @@
+idade=int(input("Digite sua idade:")) #Variável inteira
+if idade >=16: #Variável inteira
+    print("Pode votar.") #Saída permitida
+else:
+    print("Não pode votar.") #Saída não permitida
