769b9058e21ba193e8b122c3c4b0bbdbb32c808e
costazduda <dudaduddinha1407@gmail.com>
2026-09-23 12:57:26 -0300

    Add files via upload

---
+++ opcao.py
@@ -0,0 +1,11 @@
+opcao = int(input("Escolha uma opção: "))
+if opcao ==1:
+   print("Administrador")
+elif opcao ==2:
+    print("Médico")
+elif opcao ==3:
+    print("Bancário")
+elif opcao ==4:
+    print("Professor")
+else:
+    print("Opção inválida!")
