3d42ee295190029e444ddcf107d22ef876761ac0
costazduda <dudaduddinha1407@gmail.com>
2026-09-23 12:32:49 -0300

    Add files via upload

---
+++ cordosemaforo.py
@@ -0,0 +1,7 @@
+cordosemaforo = input("Qual a cor do semáforo? ")
+if cordosemaforo == "verde":
+    print("Siga em frente")
+elif cordosemaforo == "amarelo":
+    print("Atenção")
+else:
+    print("Espere o sinal ficar verde")
